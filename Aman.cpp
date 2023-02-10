334388c1aaaf4d70226a89ae6d9fd4dac72496e7
Aman Babu <72257053+aman-spp@users.noreply.github.com>
2023-02-11 03:15:14 +0530

    Leetcode Hard Problems

---
+++ Aman.cpp
@@ -0,0 +1,25 @@
+/* 
+Author - Aman Babu
+Username - alwaysAnewbie 
+Push harder than yesterday if you want a different tomorrow.
+ */
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1e9+7
+void solve(){
+    
+}
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ll test=1;
+    //cin>>test;
+    while(test--){
+        solve();
+    }
+    return 0;
+}
